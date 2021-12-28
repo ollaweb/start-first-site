2e66331f8b0c6ff817f54bdd7f7ebc61743692e8
Olga L <olla.bradford@gmail.com>
2021-12-28 13:14:01 +0300

    Burger JS done!

--- src/js/script.js
+++ src/js/script.js
@@ -6,22 +6,21 @@ function burgerSwitch() {
     const burger = document.querySelector(".burger");
     const burgerItems = document.querySelector(".burger__items");
     const menuBurger = document.querySelector(".menu__burger");
-    const menuItems = document.querySelector(".menu__items");
+    const menuItemsSmall = document.querySelector(".menu__items_small");
 
     burger.addEventListener("click", () => {
         menuBurger.classList.toggle("menu__burger_show");
         burgerItems.classList.toggle("burger__items_opened");
     });
 
-    menuItems.addEventListener("click", (event) => {
-        if (event.target == menuItems) {
-            menuBurger.classList.remove("menu__burger_show");
-            burgerItems.classList.remove("burger__items_opened");
-            console.log(event.target)
-        }
-        console.log(event.target)
+    menuItemsSmall.addEventListener("click", (event) => {
+        // if (event.target == menuItems) {
+        menuBurger.classList.remove("menu__burger_show");
+        burgerItems.classList.remove("burger__items_opened");
+        // }
+        console.log(event.target);
     });
 }
 
-
 burgerSwitch();
+
